029394176592e65a527950c89b9b7bd0ef1ced61
Shanay Murdock <murdock.shanay@gmail.com>
2021-11-24 00:10:21 -0600

    functions

--- 2-topics/finished/script.js
+++ 2-topics/finished/script.js
@@ -35,8 +35,19 @@ console.log(appleOrangeJuice);
 Number('23')
 */
 
+// Function declaration
 function calcAge1(birthYear) {
-    const age = 2037 - birthYear;
-    return age;
+    return 2037 - birthYear;
 }
 
+const age1 = calcAge1(1991);
+console.log(age1);
+
+// Function expression
+const calcAge2 = function (birthYear) {
+    return 2037 - birthYear;
+}
+
+const age2 = calcAge2(1991);
+
+console.log(age1, age2);
