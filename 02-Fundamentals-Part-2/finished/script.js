fe518e34be5e50c5df88b940bf08dfa15f195bdf
Shanay Murdock <murdock.shanay@gmail.com>
2021-11-27 20:32:20 -0600

    functions review

--- 02-Fundamentals-Part-2/finished/script.js
+++ 02-Fundamentals-Part-2/finished/script.js
@@ -161,4 +161,22 @@ console.log(yearsUntilRetirement(1950, 'Mike'));
  *******************************************
 */
 
-// CODING CHALLENGE #1
+// REVIEW FUNCTIONS
+const calcAge = function(birthYear) {
+    return 2037 - birthYear;
+}
+
+const yearsUntilRetirement = function (birthYear, firstName) {
+    const age = calcAge(birthYear);
+    const retirement = 65 - age;
+    
+    if (retirement > 0) {
+        console.log(`${firstName} will retire in ${retirement} years.`)
+        return retirement;
+    } else {
+        console.log(`${firstName} has already retired.`)
+        return -1;
+    }
+}
+console.log(yearsUntilRetirement(1991, 'Jonas'));
+console.log(yearsUntilRetirement(1970, 'Mike'));
